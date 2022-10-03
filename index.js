ce2fe849b9b97db7bf769d8e9596cf00077f0f93
Arian <arian>
2022-10-03 17:17:27 +0200

    small fixes part 2

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const markAsRead = document.getElementById("markReadButton");
 const postsNotRead = [...document.getElementsByClassName("containerPostUnread")];
 const redIcons = [...document.getElementsByClassName("redIcon")];
 
+const alertNotification = "You don't have any unread messages";
 
 markAsRead.addEventListener("click", () => {
+    if(Message.textContent === "0") return alert(alertNotification);
     postsNotRead.forEach((post) => {
         post.classList.replace("containerPostUnread", "containerPostRead");
+        post.querySelector(".redIcon").classList.remove("redIcon");
+        Message.textContent = "0";
     });
-    redIcons.forEach((icon) => {
-        icon.classList.remove("redIcon");
-    });
-    Message.textContent = "0";
 });
 
 postsNotRead.forEach((post) => {
-    post.addEventListener("click", () => {
+   post.addEventListener("click", () => {
+        if(Message.textContent === "0") return alert(alertNotification);
         post.classList.replace("containerPostUnread", "containerPostRead");
         Message.textContent = parseInt(Message.textContent) - 1;
         post.querySelector(".redIcon").classList.remove("redIcon");
     });
+    
 });
